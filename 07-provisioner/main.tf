c0916a0eff56fc4ba65c35d3c9f0cef2ef676870
Rohith M <rohith.m068@gmail.com>
2023-06-15 14:38:21 +0530

    terraform  module updated

---
+++ 07-provisioner/main.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "web" {
+  ami = data.aws_ami.example.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0de0d9214fa43e69d]
+
+  tags = {
+    Name = "Hello World"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      type = "ssh"
+      user = "centos"
+      password = "DevOps321"
+      host = self.public_ip
+    }
+
+    inline = [
+    "sudo labauto ansible",
+    "ansible-pill -i localhost, -U https://github.com/MROHITH068/roboshop-ansible.git main.yml -e role_name=frontend"]
+  }
+}
+
+data "aws_ami" "example" {
+  owners = [973714476881]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
