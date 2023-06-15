9ff6c52b4d0b03362960d60532e87020ead4b9b4
Rohith M <rohith.m068@gmail.com>
2023-06-15 14:44:00 +0530

    terraform  module updated

--- 07-provisioner/main.tf
+++ 07-provisioner/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web" {
 
     inline = [
     "sudo labauto ansible",
-    "ansible-pill -i localhost, -U https://github.com/MROHITH068/roboshop-ansible.git main.yml -e role_name=frontend"]
+    "ansible-pull -i localhost, -U https://github.com/MROHITH068/roboshop-ansible.git main.yml -e role_name=frontend"]
   }
 }
 
