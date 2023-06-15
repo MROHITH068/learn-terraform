2ee5252546c599a9a9aa05ecbddb9e1671508e44
Rohith M <rohith.m068@gmail.com>
2023-06-15 14:40:19 +0530

    terraform  module updated

--- 07-provisioner/main.tf
+++ 07-provisioner/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami = data.aws_ami.example.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [sg-0de0d9214fa43e69d]
+  vpc_security_group_ids = ["sg-0de0d9214fa43e69d"]
 
   tags = {
     Name = "Hello World"
