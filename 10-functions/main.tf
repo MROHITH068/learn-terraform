c9ed9fb0c9d014c3f087cd0aed432ad4e2d42217
Rohith M <rohith.m068@gmail.com>
2023-06-15 16:43:59 +0530

    terraform  functions updated

--- 10-functions/main.tf
+++ 10-functions/main.tf
@@ -7,3 +7,27 @@ variable "class" {
 output "classes" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = ["Mango","Apple"]
+}
+
+output "fruit_count" {
+  value = length(var.fruits)
+}
+
+variable "classes" {
+  default = {
+    devops = {
+      name = "devops"
+      topics = ["jenkins","docker"]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
