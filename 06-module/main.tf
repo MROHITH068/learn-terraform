7904c20d514c1e36c6aa1b55728347e714025f5d
Rohith M <rohith.m068@gmail.com>
2023-06-15 14:11:41 +0530

    terraform  module updated

--- 06-module/main.tf
+++ 06-module/main.tf
@@ -1,11 +1,11 @@
 module "sample1" {
   source = "./ec2"
-  name = sample1
+  name = "sample1"
 }
 
 module "sample2" {
   source = "./ec2"
-  name = sample2
+  name = "sample2"
 }
 
 output "public_ip_sample1" {
