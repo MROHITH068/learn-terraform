fa4a5b0d4440b50d0f4514114c765218df98dd24
Rohith M <rohith.m068@gmail.com>
2023-06-16 17:28:24 +0530

    terraform  for_each with roboshop modules updated with depends_on

--- 06-module/ec2/main.tf
+++ 06-module/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami = data.aws_ami.example.id
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = var.name
@@ -43,10 +43,9 @@ resource "aws_security_group" "sg" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description      = "SSH"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
@@ -55,7 +54,6 @@ resource "aws_security_group" "sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
